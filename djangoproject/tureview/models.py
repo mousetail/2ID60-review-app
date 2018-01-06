48b442e7539d74ec8b60b4ecb08f32b69fae4a46
Maurits van Riezen <MauritsVanRiezen@gmail.com>
2018-01-06 10:39:44 +0100

    added str to models

--- djangoproject/tureview/models.py
+++ djangoproject/tureview/models.py
@@ -6,20 +6,28 @@ from django.utils import timezone
 class Teacher(models.Model):
     name = models.CharField(max_length = 30)
 
+    def __str__(self):
+        return self.name
+
 class Course(models.Model):
     id = models.CharField(max_length=6, primary_key=True)
     descriptionShort = models.CharField(max_length=200)
     descriptionLong = models.TextField()
     
-    course = models.ManyToManyField(Teacher)
+    teachers = models.ManyToManyField(Teacher)
+
+    def __str__(self):
+        return self.id
 
 class Timeslot(models.Model):
     course = models.ForeignKey(Course)
     letter = models.CharField(max_length=1, choices=((i,i) for i in 'abcdex'))
     quartile = models.CharField(max_length=2,
                                 choices=tuple((str(i), str(i))
-                                              for i in range(1,5)))
+                                              for i in range(1, 5)))
     year = models.IntegerField()
+    def __str__(self):
+        return str(self.course)+" in "+str(self.year)+" Q"+str(self.quartile)+" timeslot "+str(self.letter).upper()
 
 class Student(models.Model):
     user = models.OneToOneField('auth.User', primary_key=True)
@@ -30,8 +38,10 @@ class Student(models.Model):
         ('ee', 'Electrical Engineering'),
         ('id', 'Industrial Design')
         )
-    major = models.CharField(max_length = 16)
+    major = models.CharField(max_length=16)
 
+    def __str__(self):
+        return self.user.username
 class Review(models.Model):
     student = models.ForeignKey(Student)
     course = models.ForeignKey(Course)
