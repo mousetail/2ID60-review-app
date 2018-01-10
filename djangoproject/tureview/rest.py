9b3e2f7c038e2420af4b24c001a11304e03de909
Maurits van Riezen <MauritsVanRiezen@gmail.com>
2018-01-10 18:15:40 +0100

    fixed crash because incomplete variable name change

--- djangoproject/tureview/rest.py
+++ djangoproject/tureview/rest.py
@@ -41,13 +41,13 @@ def search(request):
         timeslots = models.Timeslot.objects.all()
         timeslots = timeslots.filter(course__in=courses)
         if year != 0:
-            timeslots = filter.filter(year=year)
+            timeslots = timeslots.filter(year=year)
 
         if timeslot != "":
-            timeslots = filter.filter(letter=timeslot[0].lower())
+            timeslots = timeslots.filter(letter=timeslot[0].lower())
 
         if quartile != -1:
-            timeslots = filter.filter(quartile=quartile)
+            timeslots = timeslots.filter(quartile=quartile)
 
 
         results = timeslots
