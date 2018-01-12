c9ca412a3e84051bf3bbf96bea8c2bdfa5b6a6dd
elsaaah <e.r.wijtzes@student.tue.nl>
2018-01-12 19:35:13 +0100

    noresults added, searchresults prettified

--- djangoproject/tureview/static/javascript/search.js
+++ djangoproject/tureview/static/javascript/search.js
@@ -29,48 +29,63 @@ $("#searchform").on("submit", function(ev) {
                 function(data) {
                     var itm = $("#result");
                     itm.empty();
-                    for (var index in data){
-                        var copy = $("#courseTemplate").clone();
-                        $(copy).css("display", "")
-                        $(copy).attr("id", "cTemplate"+index)
-                        $("#cID", copy).text(data[index].id);
-                        $("#link", copy).attr("href", "/course/"+data[index].id)
-                        $("#cName", copy).text(data[index].name);
-                        $("#cShortDesc", copy).text(data[index].shortDesc);
-                        $("#cLongDesc", copy).text(data[index].longDesc);
-                        if (Number(data[index].numReviews) > 0){
-                            $("#cAvgRat", copy).text(data[index].avgRating + " ("+data[index].numReviews+" reviews)");
-                        }
-                        else {
-                            $("#cAvgRat", copy).text("no reviews")
-                        }
-
-                        for (var year in data[index].years){
-                            var yearCopy = $("#yearTemplate").clone();
-                            $(yearCopy).css("display","");
-                            $(yearCopy).attr("id", "yTemplate"+index+"y"+year);
-                            $("#yName", yearCopy).text(year+": ");
-
-                            for (var quartile in data[index].years[year]){
-                                var qCopy = $("#quartileTemplate").clone();
-                                $(qCopy).css("display","");
-                                $(qCopy).attr("id", "qTemplate"+index+"y"+year+"q"+quartile);
-
-                                $("#qName", qCopy).text(quartile);
-
-                                for (var slot in data[index].years[year][quartile]){
-                                    var tCopy = $("<span>")
-                                    tCopy.text(data[index].years[year][quartile][slot])
-                                    $("#qSlots", qCopy).append(tCopy);
-                                }
-
-                                $("#yQs", yearCopy).append(qCopy);
-                            }
-
-                            $("#cYears", copy).append(yearCopy);
-                        }
-                        itm.append(copy);
+                    console.log(data);
+                    if (data.length==0) {
+                      console.log("POEP");
+                      $("#noresults", copy).text("No results :(");
                     }
+                    else {
+                      for (var index in data){
+                          var copy = $("#courseTemplate").clone();
+                          $(copy).css("display", "")
+                          $(copy).attr("id", "cTemplate"+index)
+                          $("#cID", copy).text(data[index].id);
+                          $("#link", copy).attr("href", "/course/"+data[index].id)
+                          $("#cName", copy).text(data[index].name);
+                          $("#cShortDesc", copy).text(data[index].shortDesc);
+                          $("#cLongDesc", copy).text(data[index].longDesc);
+                          if (Number(data[index].numReviews) > 0){
+                              $("#cAvgRat", copy).text(data[index].avgRating);
+                            if (Number(data[index].numReviews) > 1){
+                              $("#aReview", copy).text(data[index].numReviews+" reviews");
+                            }
+                            else {
+                              $("#aReview", copy).text(data[index].numReviews+" review");
+                            }
+                          }
+                          else {
+                              $("#cAvgRat", copy).text("-")
+                              $("#aReview", copy).text("0 reviews");
+                          }
+
+                          for (var year in data[index].years){
+                              var yearCopy = $("#yearTemplate").clone();
+                              $(yearCopy).css("display","");
+                              $(yearCopy).attr("id", "yTemplate"+index+"y"+year);
+                              $("#yName", yearCopy).text(year+": ");
+
+                              for (var quartile in data[index].years[year]){
+                                  var qCopy = $("#quartileTemplate").clone();
+                                  $(qCopy).css("display","");
+                                  $(qCopy).attr("id", "qTemplate"+index+"y"+year+"q"+quartile);
+
+                                  $("#qName", qCopy).text(quartile);
+
+                                  for (var slot in data[index].years[year][quartile]){
+                                      var tCopy = $("<span>")
+                                      tCopy.text(data[index].years[year][quartile][slot])
+                                      $("#qSlots", qCopy).append(tCopy);
+                                  }
+
+                                  $("#yQs", yearCopy).append(qCopy);
+                              }
+
+                              $("#cYears", copy).append(yearCopy);
+                          }
+                          itm.append(copy);
+                        }
+                      }
+
                 }
             )
     }
