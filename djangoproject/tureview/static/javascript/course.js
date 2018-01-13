6635338a4342e21dc9995bfd43f177ee7015b019
codemaster97 <erik_97@live.nl>
2018-01-13 21:45:28 +0100

    Thumbs up/down functionally working on course page (still no icons)

--- djangoproject/tureview/static/javascript/course.js
+++ djangoproject/tureview/static/javascript/course.js
@@ -1,37 +1,63 @@
 $(document).ready(() => {
 
-  $('.thumbsUp').on('click', () => {
-    console.log('Thumbs Up button clicked');
+  $('.thumbsUp').on('click', event => {
+    // console.log('Thumbs Up button clicked');
     let value = $('#review-pk').val();
-    thumbs('up');
+    thumbs('up', event);
   });
 
-  $('.thumbsDown').on('click', () => {
-    console.log('Thumbs Up button clicked');
-    thumbs('down');
+  $('.thumbsDown').on('click', event => {
+    // console.log('Thumbs Up button clicked');
+    thumbs('down', event);
   });
 
 });
 
+function thumbs(upDown, event) {
+  let thumbsUp = $(event.currentTarget).siblings('#thumbsUpText').children('#thumbsUpCount');
+  let thumbsDown = $(event.currentTarget).siblings('#thumbsDownText').children('#thumbsDownCount');
+  let review_pk = $(event.currentTarget).siblings('#review-pk').val();
+  let active = $(event.currentTarget).children('.active');
+  let inactive = $(event.currentTarget).children('.inactive');
+  let other_active;
+  let other_inactive;
+  if (upDown == 'up') {
+    other_active = $(event.currentTarget).siblings('#thumbsDown').children('.active');
+    other_inactive = $(event.currentTarget).siblings('#thumbsDown').children('.inactive');
+  }
+  else {
+    other_active = $(event.currentTarget).siblings('#thumbsUp').children('.active');
+    other_inactive = $(event.currentTarget).siblings('#thumbsUp').children('.inactive');
+  }
 
-function thumbs(upDown) {
-  let review_pk = 
-  console.log("review_pk: " +
+
   $.ajax({
-      url: window.location.href + 'thumbs/',
-      type: 'POST',
-      dataType: 'json',
-      data : {
-             "thumbs": upDown,
-             "review_pk": $('#review-pk').val()
-           },
-      success(response) {
-        console.log('response: ' + response.state);
-      },
-      error(jqXHR, status, errorThrown) {
-        console.log("error");
-        console.log(jqXHR);
-
+    url: window.location.href + 'thumbs/',
+    type: 'POST',
+    dataType: 'json',
+    data : {
+      "thumbs": upDown,
+      "review_pk": review_pk
+    },
+    success(response) {
+      thumbsUp.text(response.up);
+      thumbsDown.text(response.down);
+      console.log(response.state);
+      if (response.state == 'none') { // Deactivate thumb
+        active.hide();
+        inactive.show();
       }
+      else { // Activate thumb
+        inactive.hide();
+        active.show();
+      }
+      other_active.hide(); // Deactivate other thumbs
+      other_inactive.show();
+    },
+    error(jqXHR, status, errorThrown) {
+      console.log("error");
+      console.log(jqXHR);
+
+    }
   });
 }
